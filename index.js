e7127aca1eacfe27d79e3439cf6dbf9ee81b51aa
Todd Fincannon <todd@toddfincannon.com>
2017-09-02 17:35:39 -0700

    add exports to main entry point

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+exports.DepthFirstOrder = require('./DepthFirstOrder')
+exports.Digraph = require('./Digraph')
+exports.DigraphReader = require('./DigraphReader')
+exports.DirectedCycle = require('./DirectedCycle')
+exports.SymbolDigraphReader = require('./SymbolDigraphReader')
+exports.Topological = require('./Topological')
+exports.TopologicalOrder = require('./TopologicalOrder')
